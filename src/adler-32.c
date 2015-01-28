9565608b2e8706f5ae295929c2683f06a3d75ab6
Frank Groeneveld <frank@frankgroeneveld.nl>
2015-01-28 20:26:32 +0100

    Add Adler-32 modulo for overflows

--- src/adler-32.c
+++ src/adler-32.c
@@ -1,13 +1,15 @@
 #include <stdint.h>
 
+#define ADLER_CONSTANT 65521
+
 uint32_t adler_32(uint8_t *data, int len)
 {
 	int a = 1;
 	int b = 0;
 
 	while(len--) {
-		a += *(data++);
-		b = b + a;
+		a = (a + *(data++)) % ADLER_CONSTANT;
+		b = (b + a) % ADLER_CONSTANT;
 	}
 
 	return (b << 16) | a;
--- test/adler-32.c
+++ test/adler-32.c
@@ -3,7 +3,13 @@
 
 int main()
 {
-	assert(adler_32("a", 1) == 6422626);
+	assert(adler_32((uint8_t *) "a", 1) == 6422626);
+
+	assert(adler_32((uint8_t *) "Adler-32", 8) == 0x0c34027b);
+
+	uint8_t *modulo = "pretty long Adler-32 modulo testing string";
+
+	assert(adler_32(modulo, 42) == 0x4bc00f98);
 
 	return 0;
 }
